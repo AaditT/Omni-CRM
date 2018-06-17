8c7d7e6233c46a5e2ea26d649ae6e0f28834ee10
AaditT <33333433+AaditT@users.noreply.github.com>
2018-06-17 13:47:59 -0700

    Update git_gui.py

--- git_gui.py
+++ git_gui.py
@@ -23,9 +23,6 @@ response.set("")
 def update(s):
     messagebox.showinfo("Alert",str(s))
 
-def quit():
-	root.destroy()
-    
 def get_output(cmd):
     v = subprocess.call(str(cmd))
     if str(v) == "128":
@@ -45,7 +42,6 @@ clone_entry = tk.Entry(root,width=40)
 clone_entry.grid(row=1, column=2)
 clone_button = tk.Button(root,text="Clone",command=clone_repo)
 clone_button.grid(row=1, column=3)
-quitButton = tk.Button(text=“Quit”,command=quit)
-quitButton.grid(row=5,column=0)
+
 
 root.mainloop()
